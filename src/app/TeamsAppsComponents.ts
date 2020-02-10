5e4f4896d7307c46d1b81a24c070b6e92e2cc281
Mya Thazin Aung <myathazin.aung@alfaconnections.com>
2020-02-10 12:00:31 +0630

    first commit

---
+++ src/app/TeamsAppsComponents.ts
@@ -0,0 +1,4 @@
+// Components will be added here
+export const nonce = {}; // Do not remove!
+// Automatically added for the times1MessageExtensionBot bot
+export * from "./times1MessageExtensionBot/Times1MessageExtensionBot";
---
+++ src/app/scripts/client.ts
@@ -0,0 +1,2 @@
+// Automatically added for the times1MessageExtension message extension action
+export * from "./times1MessageExtension/Times1MessageExtensionAction";
